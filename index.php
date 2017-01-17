2dc5c915a6985aa0b4a5c7d0d426ba804b5cde57
Xeronel <ripster1121@gmail.com>
2017-01-17 14:56:17 -0500

    Move navbar to header

--- index.php
+++ index.php
@@ -1,22 +1,2 @@
 <?php include_once('includes/header.html'); ?>
-
-    <nav class="navbar navbar-toggleable-sm navbar-inverse bg-inverse">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">Some Magazine!</a>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
 <?php include_once('includes/footer.html'); ?>
