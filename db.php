7a50504715736ca4fa3d4fb6148cdc4c433037d9
Xeronel <ripster1121@gmail.com>
2017-01-16 19:27:31 -0500

    Add database class

--- db.php
+++ db.php
@@ -1,5 +1,24 @@
 <?php
-$config = parse_ini_file('config.ini');
-$db = new PDO("mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8mb4", $config['db_user'], $config['db_pass']);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+class DB {
+  private $config;
+  public $db;
+
+  public function __construct() {
+    $this->config = parse_ini_file('config.ini');
+    $this->db = new PDO("mysql:host={$this->config['db_host']};" .
+                         "dbname={$this->config['db_name']};charset=utf8mb4",
+                         $this->config['db_user'], $this->config['db_pass']);
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+
+  public function fetchAll($query) {
+    return $this->db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function fetch($query) {
+    return $this->db->query($query)->fetch(PDO::FETCH_ASSOC);
+  }
+}
+
+$db = new DB();
 ?>
