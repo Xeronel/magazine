e670409076d5e735d2510db5051988654167aa7f
Xeronel <ripster1121@gmail.com>
2017-01-16 20:21:44 -0500

    Set tabwidth = 4

--- Database.class.php
+++ Database.class.php
@@ -6,54 +6,56 @@ class Database
 {
     public $db;
 
-  // Used for singleton pattern
-  private static $db_instance;
+    // Used for singleton pattern
+    private static $db_instance;
 
     public function __construct()
     {
         // Config should be a copy of config-exmaple.ini
-    // all entries are assumed to exist and be correct
-    $config = parse_ini_file('config.ini');
-    // Create a new conncetion to the database
-    $this->db = new PDO("mysql:host={$config['db_host']};" .
-                         "dbname={$config['db_name']};charset=utf8mb4",
-                         $config['db_user'], $config['db_pass']);
-    // Throw exceptions on SQL error
-    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // all entries are assumed to exist and be correct
+        $config = parse_ini_file('config.ini');
+
+        // Create a new conncetion to the database
+        $this->db = new PDO("mysql:host={$config['db_host']};" .
+                            "dbname={$config['db_name']};charset=utf8mb4",
+        $config['db_user'], $config['db_pass']);
+
+        // Throw exceptions on SQL error
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-  /**
-  * Get all query result rows
-  *
-  * @return array[]
-  */
-  public function fetchAll($query)
-  {
-      /** @var $query string */
-    return $this->db->query($query)->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  /**
-  * Get single query result row
-  *
-  * @return array[]
-  */
-  public function fetch($query)
-  {
-      return $this->db->query($query)->fetch(PDO::FETCH_ASSOC);
-  }
-
-  /**
-  * Get or create the single instance of this class
-  *
-  * @return DB
-  */
-  public static function getInstance()
-  {
-      // If an instance doesn't exist create one, else return it
-    if (!self::$db_instance) {
-        self::$db_instance = new DB();
+    /**
+    * Get all query result rows
+    *
+    * @return array[]
+    */
+    public function fetchAll($query)
+    {
+        /** @var $query string */
+        return $this->db->query($query)->fetchAll(PDO::FETCH_ASSOC);
     }
-      return self::$db_instance;
-  }
+
+    /**
+    * Get single query result row
+    *
+    * @return array[]
+    */
+    public function fetch($query)
+    {
+        return $this->db->query($query)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+    * Get or create the single instance of this class
+    *
+    * @return DB
+    */
+    public static function getInstance()
+    {
+        // If an instance doesn't exist create one, else return it
+        if (!self::$db_instance) {
+            self::$db_instance = new DB();
+        }
+        return self::$db_instance;
+    }
 }
