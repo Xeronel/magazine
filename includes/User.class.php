48680ff153ff1089b3dd94336e3100dffc2c14a6
Xeronel <ripster1121@gmail.com>
2017-01-18 10:20:20 -0500

    Fix redirect

--- includes/User.class.php
+++ includes/User.class.php
@@ -9,13 +9,13 @@ class User
     public static function login($username, $password)
     {
         $db = Database::getInstance();
-        $user = $db->fetch('SELECT * FROM users WHERE username = ?', array($username));
+        $user = $db->fetch('SELECT * FROM usersgit  WHERE username = ?', array($username));
         if (password_verify($password, $user['pwhash'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['first_name'] = $user['first_name'];
             $_SESSION['last_name'] = $user['last_name'];
-            header('Location: $_SERVER[HTTP_HOST]');
+            header("Location: {$_SERVER[HTTP_HOST]}");
             exit();
         }
     }
