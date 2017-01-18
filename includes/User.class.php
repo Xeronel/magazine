07a65ef8015f50622cae4477a5f86278463ec7c6
Xeronel <ripster1121@gmail.com>
2017-01-18 10:21:32 -0500

    Fix login redirect

--- includes/User.class.php
+++ includes/User.class.php
@@ -15,7 +15,7 @@ class User
             $_SESSION['username'] = $user['username'];
             $_SESSION['first_name'] = $user['first_name'];
             $_SESSION['last_name'] = $user['last_name'];
-            header("Location: {$_SERVER[HTTP_HOST]}");
+            header("Location: /");
             exit();
         }
     }
