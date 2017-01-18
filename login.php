a28ff85b80441a39e49b65b6fabf2a7693e4e95b
Xeronel <ripster1121@gmail.com>
2017-01-18 10:27:53 -0500

    Move styles to style.css

--- login.php
+++ login.php
@@ -18,19 +18,19 @@ if (!array_diff($requires, array_keys($_POST))) {
 
             <div class="panel-body">
                 <form id="login_form" class="form-horizontal" method="post">
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input type="text" class="form-control" name="username" value=""
                         placeholder="Enter your username">
                     </div>
 
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                         <input type="password" class="form-control" name="password"
                         placeholder="Enter your password">
                     </div>
 
-                    <div style="margin-top:10px" class="form-group">
+                    <div class="form-group">
                         <div class="col-sm-12 controls">
                             <button type="submit" class="btn btn-primary pull-right">Login</button>
                         </div>
