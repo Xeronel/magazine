a0cf17a5ff0fbe00aaaad8c4624ac039e0a217b0
Xeronel <ripster1121@gmail.com>
2017-01-18 10:18:25 -0500

    Fix typo in query

--- includes/User.class.php
+++ includes/User.class.php
@@ -9,7 +9,7 @@ class User
     public static function login($username, $password)
     {
         $db = Database::getInstance();
-        $user = $db->fetch('SELECT * FROM user WHERE username = ?', array($username));
+        $user = $db->fetch('SELECT * FROM users WHERE username = ?', array($username));
         if (password_verify($password, $user['pwhash'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
