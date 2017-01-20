80a225b6d4e0d7ede6786c3e478a54b5968bc447
Xeronel <ripster1121@gmail.com>
2017-01-20 02:21:35 -0500

    Add magazines

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
 <?php include_once('includes/header.php'); ?>
+
+<div class="row">
+    <div class="magazine col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1">
+        <div class="panel-heading">Travel + Leisure Magazine</div>
+        <div class="panel-body">
+            <div style="text-align: center;">
+                <img class="img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/71-CpdbhtUL._SL1024_.jpg">
+            </div>
+        </div>
+    </div>
+
+    <div class="magazine col-lg-5 col-md-5 col-sm-5 col-xs-5">
+        <div class="panel-heading">Food &amp; Wine Magazine</div>
+        <div class="panel-body">
+            <img class="img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/71vgmF-73LL._SL1024_.jpg">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="magazine col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1">
+        <div class="panel-heading">Brides Magazine</div>
+        <div class="panel-body">
+            <div style="text-align: center;">
+                <img class="img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/71j--D6ltdL._SL1024_.jpg">
+            </div>
+        </div>
+    </div>
+
+    <div class="magazine col-lg-5 col-md-5 col-sm-5 col-xs-5">
+        <div class="panel-heading">Real Simple Magazine</div>
+        <div class="panel-body">
+            <img class="img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/91VOLmJ%2BwDL._SL1500_.jpg">
+        </div>
+    </div>
+</div>
+
 <?php include_once('includes/footer.html'); ?>
